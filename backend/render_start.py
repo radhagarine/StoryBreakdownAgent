c1f5fc41ebeaa01a10764bdb4b357d09ae9716ee
radhagarine <radha.garine@gmail.com>
2025-04-12 12:29:52 -0700

    cleaning up

--- backend/render_start.py
+++ backend/render_start.py
@@ -5,4 +5,4 @@ from server import app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     print(f"Starting server on port {port}")
-    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info", workers=1)
